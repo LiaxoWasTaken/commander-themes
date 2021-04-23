2a62471e8fa705627e2407b14f4109ecc7dd516b
Xeiwan <57733867+Xeiwan@users.noreply.github.com>
2021-04-23 18:37:45 -0400

    Honest Dark

--- Stylesheets/Simply Dark.lua
+++ Stylesheets/Simply Dark.lua
@@ -1,55 +1,67 @@
 local module = {}
 
+--[[
+  _____      _                              _               _              _          ___  __  ___     ___  __  ___     ___  __  ___  
+ / ____|    | |                            | |             | |            | |        |__ \/_ |/ _ \   |__ \/_ |/ _ \   |__ \/_ |/ _ \ 
+| (___   ___| |_    __ _  ___ ___ ___ _ __ | |_    ___ ___ | | ___  _ __  | |_ ___      ) || | (_) |     ) || | (_) |     ) || | (_) |
+ \___ \ / _ \ __|  / _` |/ __/ __/ _ \ '_ \| __|  / __/ _ \| |/ _ \| '__| | __/ _ \    / / | |> _ <     / / | |> _ <     / / | |> _ < 
+ ____) |  __/ |_  | (_| | (_| (_|  __/ | | | |_  | (_| (_) | | (_) | |    | || (_) |  / /_ | | (_) |   / /_ | | (_) |   / /_ | | (_) |
+|_____/ \___|\__|  \__,_|\___\___\___|_| |_|\__|  \___\___/|_|\___/|_|     \__\___/  |____||_|\___( ) |____||_|\___( ) |____||_|\___/ 
+                                                                                                  |/               |/  
+]]
+
 -- Primary color
-module.ThemeColor = script.ThemeColor.Value or Color3.fromRGB(64, 157, 130)
+module.ThemeColor = Color3.fromRGB(149, 149, 149)
+
+--Other stuff
+module.TopbarColor = Color3.fromRGB(27, 27, 27)
+module.PlayersHighlightColor = Color3.fromRGB(48, 48, 48)
 
 -- Window color
 module.Window = {}
-module.Window.TopbarColor = module.ThemeColor
+module.Window.TopbarColor = module.TopbarColor
 module.Window.TopbarElementsColor = Color3.fromRGB(255, 255, 255)
-module.Window.AccentColor = Color3.fromRGB(205, 205, 205)
-module.Window.BackgroundColor = Color3.fromRGB(240, 240, 240)
+module.Window.AccentColor = Color3.fromRGB(161, 162, 162)
+module.Window.BackgroundColor = Color3.fromRGB(17, 17, 17)
 module.Window.ScrollBarColor = Color3.fromRGB(0, 0, 0)
-module.Window.TopbarBackgroundColorIfAccentUsed = Color3.fromRGB(255, 255, 255)
+module.Window.TopbarBackgroundColorIfAccentUsed = module.PlayersHighlightColor
 module.Window.TopbarElementColorIfAccentUsed = Color3.fromRGB(150, 150, 150)
-module.Window.TopbarUseAccentInstead = true
-
+module.Window.TopbarUseAccentInstead = false
 
 -- Button color
 module.Button = {}
 module.Button.RoundHoverColor = Color3.fromRGB(0, 0, 0)
 module.Button.MenuHoverColor = module.Button.RoundHoverColor
-module.Button.ModalHoverColor = module.ThemeColor
+module.Button.ModalHoverColor = Color3.fromRGB(0, 0, 0)
 
 -- Menu constructor color
 module.Menu = {}
-module.Menu.BackgroundColor = Color3.fromRGB(250, 250, 250)
-module.Menu.AccentColor = module.Window.AccentColor
-module.Menu.DefaultTextColor = Color3.fromRGB(100, 100, 100)
-module.Menu.ExitColor = module.Menu.DefaultTextColor
-
+module.Menu.BackgroundColor = module.Window.BackgroundColor
+module.Menu.AccentColor = Color3.fromRGB(161, 162, 162)
+module.Menu.DefaultTextColor = Color3.fromRGB(218, 218, 218)
+module.Menu.ExitColor = module.Window.TopbarElementsColor
 -- Home constructor color
 module.Home = {}
-module.Home.UsernameColor = Color3.fromRGB(0, 0, 0)
-module.Home.RankColor = Color3.fromRGB(120, 120, 120)
+module.Home.UsernameColor = Color3.fromRGB(209, 209, 209)
+module.Home.RankColor = Color3.fromRGB(137, 137, 137)
 module.Home.AvatarBorder = module.Window.AccentColor
-module.Home.AvatarBackground = Color3.fromRGB(255, 255, 255)
-module.Home.TopImage = "rbxasset://textures/AvatarEditorImages/AvatarEditor_LightTheme.png"
-module.Home.TopUseAccentInstead = false
-module.Home.TopBackgroundColor = Color3.fromRGB(250, 250, 250)
+module.Home.AvatarBackground = Color3.fromRGB(17, 17, 17)
+module.Home.TopImage = ""
+module.Home.TopUseAccentInstead = true
+module.Home.TopBackgroundColor = Color3.fromRGB(15, 15, 15)
 
 -- SeparatedList component color
 module.SeparatedList = {}
 module.SeparatedList.Item = {}
-module.SeparatedList.TitleColor = Color3.fromRGB(150, 150, 150)
-module.SeparatedList.Item.TitleColor = Color3.fromRGB(0, 0, 0)
-module.SeparatedList.Item.ValueColor = Color3.fromRGB(0, 0, 0)
+module.SeparatedList.TitleColor = Color3.fromRGB(209, 209, 209)
+module.SeparatedList.Item.TitleColor = Color3.fromRGB(159, 159, 159)
+module.SeparatedList.Item.ValueColor = Color3.fromRGB(209, 209, 209)
 
 -- TextField component color
 module.TextField = {}
 module.TextField.TitleColor = module.SeparatedList.TitleColor
-module.TextField.ContentColor = Color3.fromRGB(0, 0, 0)
-module.TextField.PlaceholderColor = Color3.fromRGB(150, 150, 150)
+module.TextField.ContentColor = Color3.fromRGB(255, 255, 255)
+module.TextField.PlaceholderColor = Color3.fromRGB(167, 168, 168)
 
 -- PackageButton component color
 module.PackageButton = {}
@@ -65,8 +77,8 @@ module.OverlayInput.InputBackgroundColor = module.Window.BackgroundColor
 
 -- Donate constructor color
 module.Donate = {}
-module.Donate.TitleColor = Color3.fromRGB(0, 0, 0)
-module.Donate.ParagraphColor = Color3.fromRGB(120, 120, 120)
+module.Donate.TitleColor = Color3.fromRGB(209, 209, 209)
+module.Donate.ParagraphColor = Color3.fromRGB(137, 137, 137)
 module.Donate.TopImage = module.Home.TopImage
 module.Donate.TopUseAccentInstead = module.Home.TopUseAccentInstead
 module.Donate.TopBackgroundColor = module.Home.TopBackgroundColor
